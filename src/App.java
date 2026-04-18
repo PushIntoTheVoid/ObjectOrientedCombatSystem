9df1d03c7ee3ccbf68bec11d527051dc31a740e5
KEVIN-117 <krodriguezcondori34@gmail.com>
2026-04-18 19:00:23 -0400

    feat: Add initial implementation of game classes and tests

--- src/App.java
+++ src/App.java
@@ -2,7 +2,5 @@ import models.*;
 
 public class App {
     public static void main(String[] args) {
-        // TODO: Create instances of Character, Warrior, and Archer and call their
-        // attack methods to demonstrate overriding and inheritance.
     }
 }
